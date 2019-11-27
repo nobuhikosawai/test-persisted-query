ab0ce6ef764cc3384ec82fb885625c567ed1b69f
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2019-11-27 22:27:49 +0900

    feature: use apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import gql from 'graphql-tag';
 import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { DummyContent } from './__generated__/DummyContent';
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      // uri: 'https://sjjcm90bpe.execute-api.ap-northeast-1.amazonaws.com/stg/graphql',
+      // uri: 'https://staging-preca.finc.com/graphql',
+      uri: 'http://localhost:3001/graphql',
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+
+const GET_DUMMY = gql`
+  query DummyContent{
+    dummyContents {
+      id
+    }
+  }
+`;
 
 const App: React.FC = () => {
+  const { loading, error, data } = useQuery<DummyContent>(GET_DUMMY, { client });
+
+  if (loading) return <div>'loading...'</div>;
+  if (error) return <div>Error! ${error.message}</div>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { data && data.dummyContents.map((d, idx) => <p key={idx}>{d.id}</p>)}
     </div>
   );
 }
---
+++ src/__generated__/DummyContent.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: DummyContent
+// ====================================================
+
+export interface DummyContent_dummyContents {
+  __typename: "DummyContent";
+  id: string;
+}
+
+export interface DummyContent {
+  dummyContents: DummyContent_dummyContents[];
+}
