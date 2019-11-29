ee716bb15a4e94d2e473e9f9902120d746750fff
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2019-11-29 12:51:24 +0900

    feature: use persited query

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,26 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
+import { createPersistedQueryLink } from "apollo-link-persisted-queries";
 import { DummyContent } from './__generated__/DummyContent';
 
+const link = ApolloLink.from([
+  onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        ),
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  }),
+  new HttpLink({
+    uri: 'http://localhost:3001/graphql',
+  })
+]);
+
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      // uri: 'https://sjjcm90bpe.execute-api.ap-northeast-1.amazonaws.com/stg/graphql',
-      // uri: 'https://staging-preca.finc.com/graphql',
-      uri: 'http://localhost:3001/graphql',
-    })
-  ]),
+  link: createPersistedQueryLink({ useGETForHashedQueries: true }).concat(link),
   cache: new InMemoryCache()
 });
 
